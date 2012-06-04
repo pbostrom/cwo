e6c6140d24b1b82aa06a0d34a9ac0293f6d891b8
Paul Bostrom <pbostrom@gmail.com>
2012-06-04 18:32:38 -0500

    document struct

--- src/cwo/chmgr.clj
+++ src/cwo/chmgr.clj
@@ -2,8 +2,22 @@
   (:require [lamina.core :as lamina]
             [noir.session :as session]))
 
-; nested map for assoc'ing session to handle, send/receive pair, and other-repl
-(def session-map (atom {}))
+; Channel mgmt architecture
+; Every web session has an associated channel controller
+(def sesh-id->cc (atom {}))
+
+; A channel controller is a map structure for regulating websocket traffic between clients
+; {
+;  :snd     Required, permanent channel to receive commands and shared REPLs
+;  :rec     Required, permanent channel to send commands and REPL contents
+;  :handle  Optional, anonymous users permitted
+;
+;valves are closable channels that route traffic between permanent channels
+;  :rec-valve    Optional, a subscription to a shared REPL session
+;  :trans-valve  Optional, a subscription to a REPL has been transferred,
+;                also acts as a pass-thru for other subscribers
+; }
+
 ; map to lookup session id from handle
 (def handle->sesh-id (atom {}))
 
@@ -24,19 +38,19 @@
 ;get the send-receive pair of the current session, creates if nec
 (defn get-srp []
   (let [sesh-id (session/get "sesh-id")]
-    (if-let [srp (get-in @session-map [sesh-id :srp])]
+    (if-let [srp (get-in @sesh-id->cc [sesh-id :srp])]
       srp
       (let [newsrp (sr-pair)]
         (lamina/receive-all (lamina/filter* #(.startsWith % "[") (newsrp :snd)) #(command-handler sesh-id %))
         (lamina/siphon handle-ch (newsrp :rec))
-        (swap! session-map assoc-in [sesh-id :srp] newsrp)
+        (swap! sesh-id->cc assoc-in [sesh-id :srp] newsrp)
         newsrp))))
 
 (defn broadcast []
   (let [handle (session/get "handle")
         sesh-id (session/get "sesh-id")]
       (swap! handle->sesh-id assoc handle sesh-id)
-      (swap! session-map assoc-in [sesh-id :handle] handle)
+      (swap! sesh-id->cc assoc-in [sesh-id :handle] handle)
       (lamina/enqueue handle-ch (pr-str [:addhandles [handle]]))))
 
 (defn end-broadcast []
@@ -53,20 +67,23 @@
 
 ; socket ctrl commands below
 (defn connect [sesh-id handle]
-  (let [peer-handle (get-in @session-map [sesh-id :handle] "anonymous")
-        target-srp (get-in @session-map [(@handle->sesh-id handle) :srp])
-        tmp-ch (lamina/channel)]
+  (let [peer-handle (get-in @sesh-id->cc [sesh-id :handle] "anonymous")
+        target-srp (get-in @sesh-id->cc [(@handle->sesh-id handle) :srp])
+        rec-valve (lamina/channel)]
     (println sesh-id "subscribe to" handle)
     (lamina/enqueue (target-srp :rec) (pr-str [:addpeer peer-handle]))
-    (when-let [old-ch (get-in @session-map [sesh-id :tmp-ch])]
+    (when-let [old-ch (get-in @sesh-id->cc [sesh-id :rec-valve])]
       (lamina/close old-ch))
-    (swap! session-map assoc-in [sesh-id :tmp-ch] tmp-ch)
-    (lamina/siphon (target-srp :snd) tmp-ch (get-in @session-map [sesh-id :srp :rec]))))
+    (swap! sesh-id->cc assoc-in [sesh-id :rec-valve] rec-valve)
+    (lamina/siphon (target-srp :snd) rec-valve (get-in @sesh-id->cc [sesh-id :srp :rec]))))
 
 (defn transfer [sesh-id handle]
-  (let [new-srp (@session-map (@handle->sesh-id handle))
-        tmp-ch (lamina/channel)]
-    (lamina/close (new-srp :rec))
-    (swap! new-srp assoc-in [sesh-id :tmp-ch] tmp-ch)
+  (let [new-srp (@sesh-id->cc (@handle->sesh-id handle))
+        old-srp (@sesh-id->cc sesh-id)
+        trans-valve (lamina/channel)]
+    (lamina/close (new-srp :rec-valve))
+    (when-let [old-ch (get-in @sesh-id->cc [sesh-id :trans-valve])]
+      (lamina/close old-ch))
+    (swap! sesh-id->cc assoc-in [sesh-id :trans-valve] trans-valve)
     (lamina/siphon nil nil)))
 
